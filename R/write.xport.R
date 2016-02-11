2ba0514d98daf6395c15c8202d19380797665e15
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-11 12:49:38 +0000

    Fix a bug coming from an nchar(NA) behavior change in r-devel

--- R/write.xport.R
+++ R/write.xport.R
@@ -191,10 +191,19 @@ write.xport <- function(...,
             df[[i]] <- var <- toSAS(var, format.info=formats)
 
             # compute variable length
-            if(is.character(var))
-              varLen <- max(c(8,nchar(var) ) )
-            else
+            # From R 3.3.0 NA is returned by nchar if the 
+            # the argument is NA, unless keepNA = FALSE is supplied
+            # In older versions nchar(NA) is 2, and this is the
+            # behavior we need now
+            if(is.character(var)) {
+              if ("keepNA" %in% names(as.list(args(nchar)))) {
+                varLen <- max(c(8,nchar(var, keepNA = FALSE) ) )
+              } else {
+                varLen <- max(c(8,nchar(var) ) )
+              }
+            } else {
               varLen <- 8
+            }
 
             # fill in variable offset and length information
             offsetTable[i, "len"]    <- varLen
